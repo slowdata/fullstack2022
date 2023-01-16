fd9b7ca1e44e1fea18c4eabf8422d292f469cdc6
Ricardo Dias <slo.motion@gmail.com>
2023-01-16 10:00:29 +0000

    Added eslint

---
+++ part2/phonebook/.eslintrc.js
@@ -0,0 +1,28 @@
+/* eslint-env node */
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true,
+        }
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "react/jsx-uses-react": "off",
+        "react/react-in-jsx-scope": "off",
+        "react/prop-types": "off"
+    }
+}
