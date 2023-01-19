d5efb1bd65f997507fe62cf43e4f5e2cdf09af57
Ricardo Dias <ricardo.j.dias@pgr.pt>
2023-01-19 11:40:37 +0000

    Added axios, json-server and local db for persons

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import axios from "axios";
 
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [filteredPersons, setFilteredPersons] = useState([...persons])
+  const [persons, setPersons] = useState([])
+  const [filteredPersons, setFilteredPersons] = useState([])
+
+  useEffect(() => {
+    axios.get('http://dev.local:3001/persons')
+      .then(res => {
+        setPersons(res.data)
+        setFilteredPersons(res.data)
+      })
+  }, [])
 
 
   function addPerson(p) {
